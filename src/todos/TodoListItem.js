d212a200df80066b29ff93e4074c500c803bc570
Philip Jung <philipju@usc.edu>
2020-08-28 22:08:31 -0700

    Finished redux lesson

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import './TodoListItem.css'
+import React from 'react';
+import './TodoListItem.css';
 
-const TodoListItem = ({ todo }) => {
-    return (
+const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => (
     <div className="todo-item-container">
         <h3>{todo.text}</h3>
         <div className="buttons-container">
-            <button className="completed-button">Mark As Completed</button>
-            <button className="remove-button">Remove</button>
+            {todo.isCompleted
+                ? null
+                : <button
+                    onClick={() => onCompletedPressed(todo.text)}
+                    className="completed-button">Mark As Completed</button>}
+            <button
+                onClick={() => onRemovePressed(todo.text)}
+                className="remove-button">Remove</button>
         </div>
     </div>
-    )
-}
+);
 
-export default TodoListItem
+export default TodoListItem;
